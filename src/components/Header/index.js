b878d651a0f90324ddf6f088576d71fa638a24bc
Manoj Vivek <p.manoj.vivek@gmail.com>
2020-02-16 19:28:45 +0530

    Github link added to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import cx from 'classnames';
 import styles from './style.module.css';
 import logo from '../../assets/logo.png';
+import {Icon, Tooltip} from 'antd';
 
 export default function Header() {
   return (
@@ -11,6 +12,20 @@ export default function Header() {
         className={cx(styles.logoImage)}
         alt="Strava-Github-Like-Graph"
       />
+      <div className={cx(styles.aboutSection)}>
+        <Tooltip
+          title="Lets improve it together! 😎"
+          className={cx(styles.iconContainer)}
+        >
+          <a
+            href="https://github.com/manojVivek/strava-github-like-graph"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon type="github" className={cx(styles.icon)} />
+          </a>
+        </Tooltip>
+      </div>
     </div>
   );
 }
